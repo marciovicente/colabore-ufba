b0a0043bbb8ac793dd6eac15278939d0816922d8
Marcio Vicente <marciovicente.filho@gmail.com>
2015-01-13 16:54:40 -0300

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable
+  devise :database_authenticatable, :registerable, :confirmable,
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+    def self.find_for_facebook_oauth auth, signed_in_resource=nil
+      User.where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.first_name = auth.info.first_name
+        user.last_name = auth.info.last_name
+        user.gender = auth.extra.raw_info.gender
+        # user.birth_date = auth.extra.raw_info.birthday
+        user.image = auth.info.image
+      end
+    end
+
+    def self.new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
+
+  protected
+  def confirmation_required?
+    false
+  end
+end
