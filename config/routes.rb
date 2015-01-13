d8b703dc4251982c07c82e65c43af7250abf75f4
Marcio Vicente <marciovicente.filho@gmail.com>
2015-01-13 16:55:31 -0300

    Setup devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
 
   resources :reports
 
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+
   root to: "reports#index"
 
   match "/sobre" => "global#about", via: :get
   match "/ultimas-denuncias" => "reports#list", via: :get
   match "/busca/:query" => "reports#list", via: :get
+
+  match "/admin" => "global#admin", via: :get
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
 end
